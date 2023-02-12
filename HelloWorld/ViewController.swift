b1502fa8884ebc8081a61e513603c67548076ac3
Viktor Teslenko <meex_v92@mail.ru>
2023-02-12 16:30:17 +0800

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Viktor Teslenko on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
